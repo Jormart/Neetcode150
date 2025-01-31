f49d558951a0463ec336de280c8d571a8cc462c1
Jormart <jmartineztortola@gmail.com>
2025-01-31 12:15:31 +0100

    updated submission

--- 13_ContainerWithMostWater/src/Solution.java
+++ 13_ContainerWithMostWater/src/Solution.java
@@ -1,5 +1,20 @@
 class Solution {
     public int maxArea(int[] height) {
-        
+        // Two pointer approach to solve this problem
+        int maxArea = 0;
+        int left = 0;
+        int right = height.length - 1;
+        // Loop until left and right pointers meet
+        while (left < right) {
+            int currentArea = Math.min(height[left], height[right]) * (right - left);
+            // Update maxArea if currentArea is greater
+            maxArea = Math.max(maxArea, currentArea);
+            if (height[left] < height[right]) {
+                left++;
+            } else {
+                right--;
+            }
+        }
+        return maxArea;
     }
 }
