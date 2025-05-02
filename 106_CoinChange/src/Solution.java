80d4c620d93bf753dcb7c884b3fba000c7ae465e
Jormart <jmartineztortola@gmail.com>
2025-05-02 13:49:55 +0200

    Updated Repository

--- 106_CoinChange/src/Solution.java
+++ 106_CoinChange/src/Solution.java
@@ -1,6 +1,8 @@
 // 322. Coin Change
 // https://leetcode.com/problems/coin-change/description/
 
+import java.util.Arrays;
+
 class Solution {
     // Optimal approach using Dynamic Programming
     // Time Complexity: O(n * m) where n is the amount and m is the number of coins
@@ -11,9 +13,7 @@ class Solution {
     public int coinChange(int[] coins, int amount) {
         int[] dp = new int[amount + 1];
         // Initialize the dp array with a value greater than amount
-        for (int i = 1; i <= amount; i++) {
-            dp[i] = amount + 1;
-        }
+        Arrays.fill(dp, amount + 1);
         dp[0] = 0; // Base case: 0 coins are needed to make amount 0
 
         for (int coin : coins) {
